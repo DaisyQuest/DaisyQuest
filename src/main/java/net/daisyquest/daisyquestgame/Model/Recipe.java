83a9711f2360a0cad076e127ba721c511eab2ee1
T-A-B <78736596+T-A-B@users.noreply.github.com>
2024-06-29 08:14:23 -0400

    Add discovery info for recipes

--- src/main/java/net/daisyquest/daisyquestgame/Model/Recipe.java
+++ src/main/java/net/daisyquest/daisyquestgame/Model/Recipe.java
@@ -3,6 +3,8 @@ package net.daisyquest.daisyquestgame.Model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -15,4 +17,6 @@ public class Recipe {
     private Map<String, Integer> requiredItemIdsAndAmounts;
     private Map<String, Integer> attributeRequirements;
     private String resultItemId;
+    private String discoveredBy;
+    private Date discoveryDate;
 }
